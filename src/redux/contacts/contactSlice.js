69cf059f9cd51348502f0950652d87de61939d4c
Lazyuno1987 <95475727+Lazyuno1987@users.noreply.github.com>
2022-10-11 22:08:44 +0300

    end

--- src/redux/contacts/contactSlice.js
+++ src/redux/contacts/contactSlice.js
@@ -2,7 +2,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const {REACT_APP_URL}=process.env
-console.log(REACT_APP_URL)
+
 
 export const contactApi = createApi({
   reducerPath: 'contactApi',
