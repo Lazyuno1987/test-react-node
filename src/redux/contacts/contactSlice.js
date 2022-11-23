c9a5c09f66b7b6535704c4b6e9a24587dcbcc6aa
Lazyuno1987 <95475727+Lazyuno1987@users.noreply.github.com>
2022-11-23 12:17:32 +0200

    change url

--- src/redux/contacts/contactSlice.js
+++ src/redux/contacts/contactSlice.js
@@ -6,7 +6,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const contactApi = createApi({
   reducerPath: 'contactApi',
-  baseQuery: fetchBaseQuery({ baseUrl: `https://db-for-feedback.herokuapp.com/api/feedback` }),
+     baseQuery: fetchBaseQuery({ baseUrl: `https://dull-pear-giraffe-wear.cyclic.app/api/feedback` }),
+ //baseQuery: fetchBaseQuery({ baseUrl: `http://localhost:3000/api/feedback` }),
+  //baseQuery: fetchBaseQuery({ baseUrl: `https://db-for-feedback.herokuapp.com/api/feedback` }),
   endpoints: (builder) => ({
    
       addContact: builder.mutation({
